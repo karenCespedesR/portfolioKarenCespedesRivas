fe5d80a8961a26184d5a81a7b37788de2f8d1d8d
Karen Cespedes Rivas <cesp.karen@gmail.com>
2024-11-02 16:48:42 -0300

    Comenzando el desarrollo en Vue

---
+++ vue-project/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+const nombreApellido= 'Karen Céspedes Rivas';
+</script>
+
+<template>
+  <header>
+    <h1>{{nombreApellido}}</h1>
+    <p>Bienvenido a Vue 3</p>
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
